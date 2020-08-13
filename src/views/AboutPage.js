fed0318c11b12fd25e572f9d369311d45fd7cdd1
Amalianrlt <amalianurlita97@gmail.com>
2020-08-13 16:13:46 +0700

    [Amal] - responsive

--- src/views/AboutPage.js
+++ src/views/AboutPage.js
@@ -40,37 +40,37 @@ export const AboutPage = () => {
       </section>
       <section className="reachOut">
         <Grid container>
-          <Grid item lg={7}>
+          <Grid item lg={7} xs={12}>
             <div className="connect" id="contact">
               <h1>Let's Connect</h1>
               <h4>I always open for the opportunity, so let's connect or <br/> just to say hello!</h4>
               <h2><a  href="mailto:amalianurlita97@gmail.com">amalianurlita97@gmail.com</a></h2>
             </div>
               <div className="icons">
-              <a href="https://www.linkedin.com/in/amalia-nurlita-0b0a7a153/" rel="noopener noreferrer" target="_blank"><SvgIcon component={LinkedInIcon} style={{ fontSize: 50, }} clasName="linkedin"/></a>
+              <a href="https://www.linkedin.com/in/amalia-nurlita-0b0a7a153/" rel="noopener noreferrer" target="_blank"><SvgIcon component={LinkedInIcon} style={{ fontSize: 50, }} className="linkedin"/></a>
               <a href="http://github.com/amalianrlt/" rel="noopener noreferrer" target="_blank"> <SvgIcon component={GitHubIcon} style={{ fontSize: 40 }}/></a>
             </div>
           </Grid>
-        <Grid item lg={5}>
+        <Grid item lg={5} xs={12}>
           <div className="skills">
           <h1>Skills</h1>
           <div className="coding">
             <h3>Coding</h3>
               <div className="codingChild">
-                <p><a href="" className="tagsButton">HTML/CSS</a></p>
-                <p><a href="" className="tagsButton">JavaScript</a></p>
-                <p><a href="" className="tagsButton">Git</a></p>
-                <p><a href="" className="tagsButton">ReactJs</a></p>
-                <p><a href="" className="tagsButton">CSS Libraries</a></p>
-                <p><a href="" className="tagsButton">Responsive Web Design</a></p>
+                <p><a className="tagsButton">HTML/CSS</a></p>
+                <p><a className="tagsButton">JavaScript</a></p>
+                <p><a className="tagsButton">Git</a></p>
+                <p><a className="tagsButton">ReactJs</a></p>
+                <p><a className="tagsButton">CSS Libraries</a></p>
+                <p><a className="tagsButton">Responsive Web Design</a></p>
               </div>
           </div>
           <div className="coding">
             <h3>Design</h3>
             <div className="codingChild">
-              <p><a href="" className="tagsButton">Figma</a></p>
-              <p><a href="" className="tagsButton">Adobe XD</a></p>
-              <p><a href="" className="tagsButton">Key Note</a></p>
+              <p><a className="tagsButton">Figma</a></p>
+              <p><a className="tagsButton">Adobe XD</a></p>
+              <p><a className="tagsButton">Key Note</a></p>
             </div>
             
           </div>
--- src/views/MainPage.js
+++ src/views/MainPage.js
@@ -3,39 +3,70 @@ import React from "react";
 import { Container, Grid, SvgIcon } from "@material-ui/core";
 import protra from "../styles/images/protra.svg";
 import { Link } from "react-router-dom";
-import LinkIcon from '@material-ui/icons/Link';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
-import myImg from '../styles/images/pictt.jpg'
+import LinkIcon from "@material-ui/icons/Link";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import ArrowUpwardIcon from "@material-ui/icons/ArrowUpward";
+import myImg from "../styles/images/pictt.jpg";
 
 export const MainPage = () => {
   return (
     <div className="mainPage">
       <header>
-      <Link to="/"><h1>An</h1></Link>
-      <nav className="right-side">
-       <h3><a href="#project">Projects</a></h3> 
-       <h3><a href="https://www.dropbox.com/s/zscji0weg7yodhn/Amalia%20Nurlita%20-%20Resume.pdf?dl=0" rel="noopener noreferrer" target="_blank">Resume</a></h3>
-        <Link to="/about"><h3>About</h3></Link>
-       <h3><a href="#contact">Contact</a></h3> 
-      </nav>
-    </header>
+        <Link to="/">
+          <h1>An</h1>
+        </Link>
+        <nav className="right-side">
+          <h3>
+            <a href="#project">Projects</a>
+          </h3>
+          <h3>
+            <a
+              href="https://www.dropbox.com/s/zscji0weg7yodhn/Amalia%20Nurlita%20-%20Resume.pdf?dl=0"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Resume
+            </a>
+          </h3>
+          <Link to="/about">
+            <h3>About</h3>
+          </Link>
+          <h3>
+            <a href="#contact">Contact</a>
+          </h3>
+        </nav>
+      </header>
       <section className="mainHero" id="main">
         <Container maxWidth="lg">
           <Grid container>
-
-          <Grid item xs={12} sm={8}>
-            <h1>Hi, I'm <span>Amal</span>ia Nurlita</h1>
-            <h3>
-              A Junior Frontend Developer, currently based in Indonesia. I'm
-              passionate about improving my experiences in Programming, Design and
-              constantly looking to learn new things everyday.
-            </h3>
-            <h2><a href="#project">My Work</a></h2>
-          </Grid>
-          <Grid item xs={12} sm={4}>
-            <img src={myImg} alt="myImg" className="myImg"/>
-          </Grid>
+            <Grid item xs={12} lg={8} sm={8} md={8}>
+              <h1>
+                Hi, I'm <span>Amal</span>ia Nurlita
+              </h1>
+              <h3>
+                A Junior Frontend Developer, currently based in Indonesia. I'm
+                passionate about improving my experiences in Programming, Design
+                and constantly looking to learn new things everyday.
+              </h3>
+              <div
+                className="desc"
+                style={{ display: "flex", alignItems: "center" }}
+              >
+                <h4>
+                  <a href="/about">More About Me</a>
+                </h4>
+                <h4>
+                <a
+              href="https://www.dropbox.com/s/zscji0weg7yodhn/Amalia%20Nurlita%20-%20Resume.pdf?dl=0"
+              rel="noopener noreferrer"
+              target="_blank"
+            >My Resume</a>
+                </h4>
+              </div>
+            </Grid>
+            <Grid item xs={12} lg={4} sm={4} md={4}>
+              <img src={myImg} alt="myImg" className="myImg" />
+            </Grid>
           </Grid>
         </Container>
       </section>
@@ -49,8 +80,8 @@ export const MainPage = () => {
             <h3>Solution for your Project Management</h3>
           </div>
           <div className="aboutProject">
-            <Grid item xs={7}>
-              <h2>About Protra</h2>
+            <Grid item lg={7} md={8} xs={12}>
+              <h3>About Protra</h3>
               <h4>
                 Protra is another popular project management app offering the
                 feature of project time tracking. This app gathers information
@@ -58,39 +89,73 @@ export const MainPage = () => {
                 great way to analyze your team’s performance and work progress.
               </h4>
               <div className="linkProject">
-              <a rel="noopener noreferrer" target="_blank"> <SvgIcon component={GitHubIcon} style={{ fontSize: 35 }}/></a>
-              <a href="https://protra.herokuapp.com/" rel="noopener noreferrer" target="_blank"> 
-               <h5> <span><SvgIcon component={LinkIcon} style={{ fontSize: 40 }}/></span>Live Website</h5></a>
+                <a rel="noopener noreferrer" target="_blank">
+                  {" "}
+                  <SvgIcon component={GitHubIcon} style={{ fontSize: 35 }} />
+                </a>
+                <a
+                  href="https://protra.herokuapp.com/"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <h5>
+                    {" "}
+                    <span>
+                      <SvgIcon component={LinkIcon} style={{ fontSize: 40 }} />
+                    </span>
+                    Live Website
+                  </h5>
+                </a>
               </div>
             </Grid>
           </div>
         </Container>
         <div className="protraImg">
-          <img src={protra} alt="protra" className="protraImage" />
+          <Container>
+            <img src={protra} alt="protra" className="protraImage" />
+          </Container>
         </div>
       </section>
-      <section className="otherProjects">
-      </section>
+      <section className="otherProjects"></section>
       <Container>
-      <div className="footer">
-        <h2>Contact me on : </h2>
-        <div className="socialMedia" id="contact">
-          <h3><a href="https://www.linkedin.com/in/amalia-nurlita-0b0a7a153/" rel="noopener noreferrer" target="_blank">Linkedin</a></h3>
-          <h3><a href="http://github.com/amalianrlt/" rel="noopener noreferrer" target="_blank">Github</a></h3>
-          <h3><a href="mailto:amalianurlita97@gmail.com">Email</a></h3>
+        <div className="footer">
+          <h2>Contact me on : </h2>
+          <div className="socialMedia" id="contact">
+            <h3>
+              <a
+                href="https://www.linkedin.com/in/amalia-nurlita-0b0a7a153/"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                Linkedin
+              </a>
+            </h3>
+            <h3>
+              <a
+                href="http://github.com/amalianrlt/"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                Github
+              </a>
+            </h3>
+            <h3>
+              <a href="mailto:amalianurlita97@gmail.com">Email</a>
+            </h3>
+          </div>
+          <div className="copyright">
+            <h5>
+              <strong>Amalia Nurlita</strong> - Copyright © 2020
+            </h5>
+          </div>
+          <a href="#main">
+            <div className="backTop">
+              <SvgIcon component={ArrowUpwardIcon} style={{ fontSize: 40 }} />
+              <p>Back to the top</p>
+            </div>
+          </a>
         </div>
-        <div className="copyright">
-          <h5><strong>Amalia Nurlita</strong> -  Copyright © 2020</h5>
-        </div>
-        <a href="#main">
-          
-        <div className="backTop">
-        <SvgIcon component={ArrowUpwardIcon} style={{ fontSize: 40 }}/>
-        <p>Back to the top</p>
-        </div>
-        </a>
-      </div>
-    </Container>
+      </Container>
     </div>
   );
 };
